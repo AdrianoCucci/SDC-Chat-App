739bc4e4d0b4ee8ebd94b2482594bae683f7c6f0
Adriano Cucci <cucciadriano@gmail.com>
2021-08-29 15:36:04 -0400

    Configured socketIo for app.module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { environment } from 'src/environments/environment';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+const socketIoConfig: SocketIoConfig = { url: environment.server.url };
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocketIoModule.forRoot(socketIoConfig)
   ],
   bootstrap: [AppComponent]
 })
