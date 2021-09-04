0791f0c79fc4049dba15c08a4ac96b804be2c56c
Adriano Cucci <cucciadriano@gmail.com>
2021-09-04 19:21:40 -0400

    Updated UsersList component styles.

--- src/app/shared/components/users-list/users-list.component.ts
+++ src/app/shared/components/users-list/users-list.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { User } from 'src/app/core/models/user';
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
   styleUrls: ['./users-list.component.scss']
 })
-export class UsersList implements OnInit {
+export class UsersList {
+  @Input() public users: User[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public getOnlineUsers(): User[] {
+    return this.users?.filter(u => u.isOnline) ?? null;
   }
 
+  public getOfflineUsers(): User[] {
+    return this.users?.filter(u => !u.isOnline) ?? null;
+  }
+
+  public getUserInitials(user: User): string {
+    return `${user.firstName.charAt(0)}${user.lastName.charAt(0)}`.trim().toUpperCase();
+  }
+
+  public getUserFullName(user: User): string {
+    return `${user.firstName} ${user.lastName}`.trim();
+  }
 }
