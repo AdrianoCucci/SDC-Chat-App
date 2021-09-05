58f5646021c050b28e13f25bc919163ba10ef006
Adriano Cucci <cucciadriano@gmail.com>
2021-09-05 12:03:38 -0400

    Added InputText component.

---
+++ src/app/shared/components/forms/inputs/input-text/input-text-validations.ts
@@ -0,0 +1,9 @@
+import { FormInputValidations } from "../../base/form-input-validations";
+
+export interface InputTextValidations extends FormInputValidations {
+  pattern?: RegExp | string;
+  min?: number;
+  max?: number;
+  minLength?: number;
+  maxLength?: number;
+}
