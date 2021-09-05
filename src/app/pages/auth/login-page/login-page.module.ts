dbf17434a6753d0854fe5765bd651808bc16f460
Adriano Cucci <cucciadriano@gmail.com>
2021-09-05 14:47:49 -0400

    Added basic login-page content.

--- src/app/pages/auth/login-page/login-page.module.ts
+++ src/app/pages/auth/login-page/login-page.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { LoginPageRoutingModule } from './login-page-routing.module';
+
+import { FormModule } from 'src/app/shared/components/forms/form/form.module';
+import { InputTextModule } from 'src/app/shared/components/forms/inputs/input-text/input-text.module';
+import { ButtonModule } from 'src/app/shared/components/button/button.module';
+
 import { LoginPageComponent } from './login-page.component';
 
 @NgModule({
   declarations: [LoginPageComponent],
   imports: [
     CommonModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    FormModule,
+    InputTextModule,
+    ButtonModule
   ]
 })
 export class LoginPageModule { }
