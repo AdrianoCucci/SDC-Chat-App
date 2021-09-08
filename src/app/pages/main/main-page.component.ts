494b5647aa1cf6a03c922ea543ba7606b7005e85
Adriano Cucci <cucciadriano@gmail.com>
2021-09-08 16:23:15 -0400

    Implemented content of MainPage.

--- src/app/pages/main/main-page.component.ts
+++ src/app/pages/main/main-page.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/core/services/login.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss']
 })
-export class MainPage implements OnInit {
+export class MainPage {
+  public readonly appName: string = environment.app.name;
+  public readonly appVersion: string = environment.app.version;
+  public readonly userDisplayName: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public logoutDialogVisible: boolean = false;
+  
+  constructor(private _loginService: LoginService, private _router: Router) { 
+    this.userDisplayName = _loginService.user.displayName || _loginService.user.username;
   }
 
+  public logout(): void {
+    this._loginService.logout();
+    this._router.navigateByUrl("");
+  }
 }
--- src/app/pages/main/main-page.module.ts
+++ src/app/pages/main/main-page.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageRoutingModule } from './main-page-routing.module';
 
+import { ButtonModule } from 'src/app/shared/components/button/button.module';
+import { PopoverModule } from 'src/app/shared/components/popover/popover.module';
+import { DialogModule } from 'src/app/shared/components/dialog/dialog.module';
+
 import { MainPage } from './main-page.component';
 
 @NgModule({
   declarations: [MainPage],
   imports: [
     CommonModule,
-    MainPageRoutingModule
+    MainPageRoutingModule,
+    ButtonModule,
+    PopoverModule,
+    DialogModule
   ]
 })
 export class MainPageModule { }
