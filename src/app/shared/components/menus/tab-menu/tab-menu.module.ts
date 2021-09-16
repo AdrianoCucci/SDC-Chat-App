c651d4326855d0e2bad305bbf531cfc6043a6ca7
Adriano Cucci <cucciadriano@gmail.com>
2021-09-16 13:19:03 -0400

    Updated style/layout of TabMenu component.

--- src/app/shared/components/menus/tab-menu/tab-menu.module.ts
+++ src/app/shared/components/menus/tab-menu/tab-menu.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { TabMenu } from './tab-menu.component';
 const component = [TabMenu];
@@ -8,7 +9,8 @@ const component = [TabMenu];
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    FontAwesomeModule
   ],
   exports: component,
   declarations: component
