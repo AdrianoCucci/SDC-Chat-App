5590aebaa48c88aa598a9e8001455f9101e6bdd2
Adriano Cucci <cucciadriano@gmail.com>
2021-09-16 18:00:23 -0400

    Added sorting to Table component.

--- src/app/shared/components/tables/table/table.component.ts
+++ src/app/shared/components/tables/table/table.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { faSort, faSortAmountDown, faSortAmountUp, IconDefinition, IconLookup } from '@fortawesome/free-solid-svg-icons';
+import { TableCell } from './table-cell';
 import { TableConfig } from './table-config';
 
 @Component({
@@ -7,5 +9,56 @@ import { TableConfig } from './table-config';
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent {
+  public readonly sortIcon: IconDefinition = faSort;
+  public readonly sortIconUp: IconDefinition = faSortAmountUp;
+  public readonly sortIconDown: IconDefinition = faSortAmountDown;
+
+  private _currentSortHeader: HTMLElement;
+
   @Input() public config: TableConfig;
+
+  onSort(cell: TableCell, element: HTMLElement): void {
+    if(cell.sortable) {
+      this.updateSortHeaderElement(element);
+      this.sortCellData(cell, element.getAttribute("sort") === "asc");
+    }
+  }
+
+  private updateSortHeaderElement(element: HTMLElement) {
+    if(this._currentSortHeader != null && element !== this._currentSortHeader) {
+      this._currentSortHeader.removeAttribute("sort");
+    }
+
+    if(element.getAttribute("sort") === "asc") {
+      element.setAttribute("sort", "desc");
+    }
+    else {
+      element.setAttribute("sort", "asc");
+    }
+
+    this._currentSortHeader = element;
+  }
+
+  private sortCellData(cell: TableCell, ascending: boolean) {
+    const ascendingMultiplier: number = ascending ? 1 : -1;
+
+    this.config.data.sort((a: any, b: any) => {
+      let result: number;
+
+      const aValue: any = a[cell.valueField];
+      const bValue: any = b[cell.valueField];
+
+      if(aValue < bValue) {
+        result = ascending ? -1 : 1;
+      }
+      else if(bValue > aValue) {
+        result = ascending ? 1 : -1;
+      }
+      else {
+        result = 0;
+      }
+
+      return result;
+    });
+  }
 }
--- src/app/shared/components/tables/table/table.module.ts
+++ src/app/shared/components/tables/table/table.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { InputTextModule } from '../../forms/inputs/input-text/input-text.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { TableComponent } from './table.component';
 const component = [TableComponent];
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    InputTextModule,
+    FontAwesomeModule
+  ],
   exports: component,
   declarations: component
 })
