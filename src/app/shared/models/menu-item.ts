e77c3e16b6e03c383ab53faff6b05c32b485d325
Adriano Cucci <cucciadriano@gmail.com>
2021-09-16 10:49:07 -0400

    Updated MenuItem model.

--- src/app/shared/models/menu-item.ts
+++ src/app/shared/models/menu-item.ts
@@ -3,10 +3,12 @@ import { IconDefinition } from "@fortawesome/fontawesome-common-types";
 export interface MenuItem {
   label?: string;
   icon?: IconDefinition;
+  description?: string;
   routerLink?: string;
   routerLinkActive?: string;
   id?: string;
   cssClass?: string;
+  data?: any;
   onClick?: (context: any) => void;
   children?: MenuItem[];
 }
