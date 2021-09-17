adc7aa5d48157fe798dcc62c0706dda53fc5aead
Adriano Cucci <cucciadriano@gmail.com>
2021-09-17 14:56:27 -0400

    Updated style/layout of Table component.

--- src/app/shared/components/tables/table/table.component.ts
+++ src/app/shared/components/tables/table/table.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { IconDefinition } from '@fortawesome/fontawesome-common-types';
 import { faSort, faSortAmountDown, faSortAmountUp } from '@fortawesome/free-solid-svg-icons';
+import { ColumnMode } from '@swimlane/ngx-datatable';
 import { TableCell } from './table-cell';
 
 @Component({
@@ -11,6 +12,7 @@ import { TableCell } from './table-cell';
 export class TableComponent {
   @Input() public cells: TableCell[];
   @Input() public data: any[];
+  @Input() public columnMode = ColumnMode.force;
 
   public readonly sortNoneIcon: IconDefinition = faSort;
   public readonly sortAscIcon: IconDefinition = faSortAmountUp;
