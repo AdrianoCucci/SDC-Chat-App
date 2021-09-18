e5cb16e301e90dd7d3e4b0a1ea1c718446c50929
Adriano Cucci <cucciadriano@gmail.com>
2021-09-17 21:09:09 -0400

    Added getCommonFaIcon utility function.

---
+++ src/app/shared/functions/get-common-fa-icon.ts
@@ -0,0 +1,33 @@
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import { faPlus, faTimes, faCheck, faTrash, faSignInAlt, faSignOutAlt, faUpload, faDownload, faCloudUploadAlt, faCloudDownloadAlt, faChevronUp, faChevronDown, faChevronLeft, faChevronRight, faUser, faUsers, faUserPlus, faUserEdit, faLock, faKey, faExternalLinkAlt, faEdit, faPen, faSync, faCog, faCogs } from "@fortawesome/free-solid-svg-icons";
+
+const commonIconsMap = new Map<string, IconDefinition>([
+  ["plus", faPlus],
+  ["times", faTimes],
+  ["check", faCheck],
+  ["trash", faTrash],
+  ["login", faSignInAlt],
+  ["logout", faSignOutAlt],
+  ["upload", faUpload],
+  ["download", faDownload],
+  ["cloud-upload", faCloudUploadAlt],
+  ["cloud-download", faCloudDownloadAlt],
+  ["chevron-up", faChevronUp],
+  ["chevron-down", faChevronDown],
+  ["chevron-left", faChevronLeft],
+  ["chevron-right", faChevronRight],
+  ["user", faUser],
+  ["users", faUsers],
+  ["user-plus", faUserPlus],
+  ["user-edit", faUserEdit],
+  ["lock", faLock],
+  ["key", faKey],
+  ["external-link", faExternalLinkAlt],
+  ["edit", faEdit],
+  ["pen", faPen],
+  ["sync", faSync],
+  ["cog", faCog],
+  ["cogs", faCogs]
+]);
+
+export const getCommonFaIcon = (icon: string): IconDefinition => commonIconsMap.get(icon);
