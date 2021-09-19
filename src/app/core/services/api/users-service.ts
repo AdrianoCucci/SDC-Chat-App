6ed565dbf7c68239ab67dde817091c73e73fe5c4
Adriano Cucci <cucciadriano@gmail.com>
2021-09-19 11:48:56 -0400

    Updated UsersService to include all API methods.

--- src/app/core/services/api/users-service.ts
+++ src/app/core/services/api/users-service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { User } from "../../models/users/user";
 import { UserParams } from "../../models/users/user-params";
+import { UserRequest } from "../../models/users/user-request";
 import { WebApiService } from "./web-api.service";
 
 @Injectable({
@@ -20,5 +21,21 @@ export class UsersService extends WebApiService {
     const url: string = `${this._apiPrefix}${query}`;
 
     return this.get(url);
+  }
+
+  public getUser(id: number): Observable<HttpResponse<User>> {
+    return this.get(`${this._apiPrefix}/${id}`);
+  }
+
+  public addUser(request: UserRequest): Observable<HttpResponse<User>> {
+    return this.post(this._apiPrefix, request);
+  }
+
+  public updateUser(id: number, request: UserRequest): Observable<HttpResponse<User>> {
+    return this.put(`${this._apiPrefix}/${id}`, request);
+  }
+
+  public deleteUser(id: number): Observable<HttpResponse<void>> {
+    return this.delete(`${this._apiPrefix}/${id}`);
   }
 }
