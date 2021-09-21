e80d262571f360a68ceb662efcf871d0a55c6ac6
Adriano Cucci <cucciadriano@gmail.com>
2021-09-21 13:42:51 -0400

    Updated parseHttpError to allow not returning a string array.

--- src/app/shared/functions/parse-http-error.ts
+++ src/app/shared/functions/parse-http-error.ts
@@ -1,10 +1,13 @@
 import { HttpErrorResponse } from "@angular/common/http";
 
-export const parseHttpError = (httpError: HttpErrorResponse): string | string[] => {
+export const parseHttpError = (httpError: HttpErrorResponse, noArray: boolean = false): string | string[] => {
   let message: any = httpError.error.message;
 
   if(!message) {
     message = httpError.error.message;
+  }
+  else if(noArray && Array.isArray(message)) {
+    message = "A server error has occurred";
   }
 
   return message;
