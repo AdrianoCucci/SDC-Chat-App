b07fa2794d29f3315171a600b12543f985e02dc7
Adriano Cucci <cucciadriano@gmail.com>
2021-09-21 14:47:53 -0400

    Implemented OrganizationsTable in OrganizationsPage.

--- src/app/pages/main/organizations-page/organizations-page.component.ts
+++ src/app/pages/main/organizations-page/organizations-page.component.ts
@@ -1,4 +1,8 @@
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Organization } from 'src/app/core/models/organizations/organization';
+import { OrganizationsService } from 'src/app/core/services/api/organizations-service';
+import { parseHttpError } from 'src/app/shared/functions/parse-http-error';
 
 @Component({
   selector: 'app-organizations-page',
@@ -9,10 +13,28 @@ export class OrganizationsPage implements OnInit {
   public loadingVisible: boolean = false;
   public loadError: string;
   public errorVisible: boolean = false;
-  
-  constructor() { }
 
-  ngOnInit(): void {
+  private _organizations: Organization[];
+
+  constructor(private _orgsService: OrganizationsService) { }
+
+  async ngOnInit(): Promise<void> {
+    try {
+      this.loadingVisible = true;
+
+      const response: HttpResponse<Organization[]> = await this._orgsService.getAllOrganizations().toPromise();
+      this._organizations = response.body;
+    }
+    catch(error) {
+      this.loadError = parseHttpError(error as HttpErrorResponse, true) as string;
+      this.errorVisible = true;
+    }
+    finally {
+      this.loadingVisible = false;
+    }
   }
 
+  public get organizations(): Organization[] {
+    return this._organizations;
+  }
 }
--- src/app/pages/main/organizations-page/organizations-page.module.ts
+++ src/app/pages/main/organizations-page/organizations-page.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrganizationsPageRoutingModule } from './organizations-page-routing.module';
 
+import { OrganizationsTableModule } from 'src/app/shared/components/tables/organizations-table/organizations-table.module';
 import { ButtonModule } from 'src/app/shared/components/button/button.module';
 import { DialogModule } from 'src/app/shared/components/dialog/dialog.module';
 import { LoaderModule } from 'src/app/shared/components/loader/loader.module';
@@ -13,6 +14,7 @@ import { OrganizationsPage } from './organizations-page.component';
   imports: [
     CommonModule,
     OrganizationsPageRoutingModule,
+    OrganizationsTableModule,
     LoaderModule,
     DialogModule,
     ButtonModule
