11edcc3f79e8a18134def33dac4d04f8f0271fc2
Adriano Cucci <cucciadriano@gmail.com>
2021-09-22 10:31:23 -0400

    Added IVisibilityChangeable interface.

---
+++ src/app/shared/interfaces/i-visibility-changeable.ts
@@ -0,0 +1,15 @@
+import { EventEmitter } from "@angular/core";
+
+export interface IVisibilityChangeable {
+  visibleChange: EventEmitter<boolean>;
+  onShow: EventEmitter<void>;
+  onHide: EventEmitter<void>;
+
+  visible: boolean;
+
+  show(params?: any): void;
+
+  hide(params?: any): void;
+
+  toggleVisible(params?: any): void;
+}
