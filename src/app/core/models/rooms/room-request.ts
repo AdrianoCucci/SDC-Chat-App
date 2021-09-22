f2651bee4d3a3cc961b3fd0dbad52def42216f6e
Adriano Cucci <cucciadriano@gmail.com>
2021-09-22 13:11:21 -0400

    Updated Room models to include RoomPingSound.

--- src/app/core/models/rooms/room-request.ts
+++ src/app/core/models/rooms/room-request.ts
@@ -1,7 +1,10 @@
+import { RoomPingSound } from "./room-ping-sound";
+
 export interface RoomRequest {
   id?: number;
   name: string;
   number?: number;
   description?: string;
+  pingSound?: RoomPingSound;
   organizationId: number;
 }
--- src/app/core/models/rooms/room.ts
+++ src/app/core/models/rooms/room.ts
@@ -1,10 +1,12 @@
 import { Organization } from "../organizations/organization";
+import { RoomPingSound } from "./room-ping-sound";
 
 export interface Room {
   id: number;
   name: string;
   number?: number;
   description?: string;
+  pingSound?: RoomPingSound;
   organizationId: number;
 
   organization?: Organization;
