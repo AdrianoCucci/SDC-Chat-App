d78854d6cb0effbaa8afcd2d147fc3f34518ba7a
Adriano Cucci <cucciadriano@gmail.com>
2021-09-22 12:36:53 -0400

    Moved TabMenu component into /modules/shared

--- src/app/pages/main/main-page.module.ts
+++ src/app/pages/main/main-page.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageRoutingModule } from './main-page-routing.module';
 
-import { TabMenuModule } from 'src/app/shared/components/menus/tab-menu/tab-menu.module';
+import { TabMenuModule } from 'src/app/modules/shared/menus/tab-menu/tab-menu.module';
 import { ButtonModule } from 'src/app/modules/shared/button/button.module';
 import { PopoverModule } from 'src/app/modules/shared/overlays/popover/popover.module';
 import { DialogModule } from 'src/app/modules/shared/overlays/dialog/dialog.module';
