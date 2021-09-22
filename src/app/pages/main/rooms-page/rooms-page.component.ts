2f4d8e735bed58d47e5ba0498c422b70f55d732d
Adriano Cucci <cucciadriano@gmail.com>
2021-09-22 16:01:15 -0400

    Implemented RoomsPage.

--- src/app/pages/main/rooms-page/rooms-page.component.ts
+++ src/app/pages/main/rooms-page/rooms-page.component.ts
@@ -1,4 +1,9 @@
+import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Room } from 'src/app/core/models/rooms/room';
+import { RoomsService } from 'src/app/core/services/api/rooms-service';
+import { LoginService } from 'src/app/core/services/login.service';
+import { parseHttpError } from 'src/app/shared/functions/parse-http-error';
 
 @Component({
   selector: 'app-rooms-page',
@@ -6,10 +11,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rooms-page.component.scss']
 })
 export class RoomsPage implements OnInit {
+  public readonly organizationId: number;
 
-  constructor() { }
+  public loadingVisible: boolean = false;
+  public loadError: string;
+  public errorVisible: boolean = false;
 
-  ngOnInit(): void {
+  private _rooms: Room[];
+
+  constructor(private _roomsService: RoomsService, loginService: LoginService) {
+    this.organizationId = loginService.user.organizationId;
   }
 
+  async ngOnInit(): Promise<void> {
+    try {
+      this.loadingVisible = true;
+
+      const response: HttpResponse<Room[]> = await this._roomsService.getAllRooms({ organizationId: this.organizationId }).toPromise();
+      this._rooms = response.body;
+    }
+    catch(error) {
+      this.loadError = parseHttpError(error as HttpErrorResponse, true) as string;
+      this.errorVisible = true;
+    }
+    finally {
+      this.loadingVisible = false;
+    }
+  }
+
+  public get rooms(): Room[] {
+    return this._rooms;
+  }
 }
--- src/app/pages/main/rooms-page/rooms-page.module.ts
+++ src/app/pages/main/rooms-page/rooms-page.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RoomsPageRoutingModule } from './rooms-page-routing.module';
 
+import { RoomsTableModule } from 'src/app/core/modules/tables/rooms-table/rooms-table.module';
+import { ButtonModule } from 'src/app/shared/modules/button/button.module';
+import { DialogModule } from 'src/app/shared/modules/overlays/dialog/dialog.module';
+import { LoaderModule } from 'src/app/shared/modules/overlays/loader/loader.module';
+
 import { RoomsPage } from './rooms-page.component';
 
 @NgModule({
   declarations: [RoomsPage],
   imports: [
     CommonModule,
-    RoomsPageRoutingModule
+    RoomsPageRoutingModule,
+    RoomsTableModule,
+    LoaderModule,
+    DialogModule,
+    ButtonModule
   ]
 })
 export class RoomsPageModule { }
