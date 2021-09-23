b8dc7263699de6342cf881be266592e659843c5e
Adriano Cucci <cucciadriano@gmail.com>
2021-09-23 15:43:38 -0400

    Replaced current InputSelect with new InputSelect.

--- src/app/shared/modules/forms/inputs/input-select/input-select.module.ts
+++ src/app/shared/modules/forms/inputs/input-select/input-select.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { ButtonModule } from '../../../button/button.module';
+import { PopoverModule } from '../../../overlays/popover/popover.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { InputSelect } from './input-select.component';
 const component = [InputSelect];
@@ -11,7 +13,9 @@ const component = [InputSelect];
   imports: [
     CommonModule,
     FormsModule,
-    ButtonModule
+    ButtonModule,
+    PopoverModule,
+    FontAwesomeModule
   ],
   exports: component,
   declarations: component
