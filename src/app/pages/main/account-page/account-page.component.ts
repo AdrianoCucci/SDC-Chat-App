cbdfdf23b95bd13c0c34ff57c06ae5bba9c182d8
Adriano Cucci <cucciadriano@gmail.com>
2021-09-25 19:03:22 -0400

    Added content to AccountPage.

--- src/app/pages/main/account-page/account-page.component.ts
+++ src/app/pages/main/account-page/account-page.component.ts
@@ -1,4 +1,12 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Role } from 'src/app/core/models/auth/role';
+import { Organization } from 'src/app/core/models/organizations/organization';
+import { User } from 'src/app/core/models/users/user';
+import { OrganizationsService } from 'src/app/core/services/api/organizations-service';
+import { LoginService } from 'src/app/core/services/login.service';
+import { enumToPairs } from 'src/app/shared/functions/enum-to-pairs';
+import { Pair } from 'src/app/shared/models/pair';
 
 @Component({
   selector: 'app-account-page',
@@ -6,10 +14,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-page.component.scss']
 })
 export class AccountPage implements OnInit {
+  public readonly clientUser: User;
+  public readonly rolePairs: Pair<string, Role>[] = enumToPairs(Role, true);
 
-  constructor() { }
+  private _organization: Organization;
+  private _loadingVisible: boolean = false;
 
-  ngOnInit(): void {
+  constructor(private _orgsService: OrganizationsService, loginService: LoginService) {
+    this.clientUser = loginService.user;
   }
 
+  public getRoleName(role: Role): string {
+    return this.rolePairs.find((p: Pair) => p.value === role)?.key ?? null;
+  }
+
+  async ngOnInit(): Promise<void> {
+    if(this.clientUser.organizationId != null) {
+      try {
+        this._loadingVisible = true;
+        this._organization = await this.loadOrganization(this.clientUser.organizationId);
+      }
+      catch(error) {
+        console.error(error);
+      }
+      finally {
+        this._loadingVisible = false;
+      }
+    }
+  }
+
+  private loadOrganization(id: number): Promise<Organization> {
+    return new Promise<Organization>(async (resolve, reject) => {
+      try {
+        const response: HttpResponse<Organization> = await this._orgsService.getOrganization(id).toPromise();
+        resolve(response.body);
+      }
+      catch(error) {
+        reject(error);
+      }
+    });
+  }
+
+  public get organization(): Organization {
+    return this._organization;
+  }
+
+  public get loadingVisible(): boolean {
+    return this._loadingVisible;
+  }
 }
--- src/app/pages/main/account-page/account-page.module.ts
+++ src/app/pages/main/account-page/account-page.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountPageRoutingModule } from './account-page-routing.module';
 
+import { ButtonModule } from 'src/app/shared/modules/button/button.module';
+import { LoaderModule } from 'src/app/shared/modules/overlays/loader/loader.module';
+
 import { AccountPage } from './account-page.component';
 
 @NgModule({
   declarations: [AccountPage],
   imports: [
     CommonModule,
-    AccountPageRoutingModule
+    AccountPageRoutingModule,
+    ButtonModule,
+    LoaderModule
   ]
 })
 export class AccountPageModule { }
