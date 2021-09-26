bfba6adaa540d6dd2a3a08885f3fc49b6d20ef5a
Adriano Cucci <cucciadriano@gmail.com>
2021-09-26 13:19:45 -0400

    Implemented filtering features in FilterList.

--- src/app/shared/modules/filter-list/filter-list.component.ts
+++ src/app/shared/modules/filter-list/filter-list.component.ts
@@ -9,7 +9,12 @@ import { TemplateDirective } from '../directives/template.directive';
 export class FilterList implements AfterViewInit {
   @Input() public items: any[];
   @Input() public keywordFields: string | string[];
+  @Input() public options: FilterListOptions = {
+    caseSensitive: false,
+    exactMatch: false
+  };
 
+  private _filteredItems: any[];
   private _itemTemplate: TemplateRef<any>;
 
   @ContentChild(TemplateDirective) private readonly _itemTemplateDirective: TemplateDirective;
@@ -18,7 +23,75 @@ export class FilterList implements AfterViewInit {
     setTimeout(() => this._itemTemplate = this._itemTemplateDirective?.template);
   }
 
+  public filter(value: string): any[] {
+    const filteredItems: any[] = [];
+
+    if(this.hasItems && this.keywordFields) {
+      const keywordFields: string[] = Array.isArray(this.keywordFields) ? this.keywordFields : [this.keywordFields];
+
+      for(let i = 0; i < this.items.length; i++) {
+        const item: any = this.items[i];
+
+        if(!this.itemHasKeyword(item, value, keywordFields, this.options)) {
+          filteredItems.push(item);
+        }
+      }
+    }
+
+    this._filteredItems = filteredItems;
+    return this._filteredItems;
+  }
+
+  public clearFilter(): void {
+    this._filteredItems = null;
+  }
+
+  private itemHasKeyword(item: any, value: string, keywordFields: string[], options?: FilterListOptions): boolean {
+    let hasKeyword: boolean = false;
+
+    const isMatch = (a: string, b: string): boolean => {
+      if(options == null || !options.caseSensitive) {
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+      }
+
+      return options == null || !options.exactMatch ? a.includes(b) : a === b;
+    };
+
+    for(let i = 0; i < keywordFields.length; i++) {
+      let fieldValue: string = item[keywordFields[i]];
+
+      if(fieldValue != null && isMatch(fieldValue.toString(), value)) {
+        hasKeyword = true;
+        break;
+      }
+    }
+
+    return hasKeyword;
+  }
+
+  public isItemFiltered(item: any): boolean {
+    return this._filteredItems?.includes(item);
+  }
+
+  public get hasItems(): boolean {
+    return this.items?.length > 0 ?? false;
+  }
+
+  public get filteredItems(): any[] {
+    return this._filteredItems;
+  }
+
+  public get hasFilteredItems(): boolean {
+    return this._filteredItems?.length > 0 ?? false;
+  }
+
   public get itemTemplate(): TemplateRef<any> {
     return this._itemTemplate;
   }
+}
+
+export interface FilterListOptions {
+  caseSensitive?: boolean;
+  exactMatch?: boolean;
 }
