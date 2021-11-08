62918681406ac81566a85c9b6b1ea2452f80c735
Adriano Cucci <cucciadriano@gmail.com>
2021-11-08 13:39:26 -0500

    Added password-reset functions in AuthService.

--- src/app/core/services/api/auth-service.ts
+++ src/app/core/services/api/auth-service.ts
@@ -1,8 +1,10 @@
 import { HttpClient, HttpResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { AdminPassResetRequest } from "../../models/auth/admin-pass-reset-request";
 import { AuthRequest } from "../../models/auth/auth-request";
 import { AuthResponse } from "../../models/auth/auth-response";
+import { PassResetRequest } from "../../models/auth/pass-reset-request";
 import { WebApiService } from "./web-api.service";
 
 @Injectable({
@@ -17,5 +19,13 @@ export class AuthService extends WebApiService {
 
   public login(request: AuthRequest): Observable<HttpResponse<AuthResponse>> {
     return super.post(`${this._apiPrefix}/login`, request);
+  }
+
+  public resetPassword(request: PassResetRequest): Observable<HttpResponse<void>> {
+    return this.post(`${this._apiPrefix}/reset-password`, request);
+  }
+
+  public adminResetPassword(request: AdminPassResetRequest): Observable<HttpResponse<void>> {
+    return this.post(`${this._apiPrefix}/admin-reset-password`, request);
   }
 }
