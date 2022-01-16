beae0e1a92c899048cabc2b4c151efcd7b5f8e55
Adriano Cucci <cucciadriano@gmail.com>
2022-01-16 15:20:24 -0500

    Added Pagination-related models.

---
+++ src/app/shared/models/pagination/paginate-meta.ts
@@ -0,0 +1,15 @@
+import { Paginatable } from "./paginatable";
+
+export interface PaginateMeta extends Paginatable {
+  readonly skip?: number;
+  readonly take?: number;
+
+  readonly itemsCount: number;
+  readonly totalItemsCount: number;
+
+  readonly currentPage: number;
+  readonly totalPages: number;
+
+  readonly hasNext: boolean;
+  readonly hasPrevious: boolean;
+}
