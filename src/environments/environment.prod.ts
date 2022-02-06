80c73993d07309ac3a5c33a2319196186b662453
Adriano Cucci <cucciadriano@gmail.com>
2022-02-06 15:52:03 -0500

    Updated configuration.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,11 @@ import { environment as shared } from "./environment.shared";
 
 export const environment = {
   ...shared,
-  production: true
+
+  production: true,
+
+  server: {
+    ...shared.server,
+    host: "http://192.168.0.125:3000"
+  }
 };
