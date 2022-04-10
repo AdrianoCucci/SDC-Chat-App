821b60d4ec494c4efc40dca60140c68572bd02ad
acucci <acucci@edgecollab.com>
2022-04-10 19:21:27 -0400

    Refactored common styles into component.

--- src/app/pages/main/account-page/account-details/account-details.module.ts
+++ src/app/pages/main/account-page/account-details/account-details.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountDetailsRoutingModule } from './account-details-routing.module';
 
+import { InfoRowModule } from 'src/app/shared/modules/info-row/info-row.module';
 import { ButtonModule } from 'src/app/shared/modules/button/button.module';
 import { AccountFormModule } from 'src/app/core/modules/forms/account-form/account-form.module';
 import { PassChangeFormModule } from 'src/app/core/modules/forms/pass-change-form/pass-change-form.module';
@@ -13,6 +14,7 @@ import { AccountDetailsPage } from './account-details.page';
   imports: [
     CommonModule,
     AccountDetailsRoutingModule,
+    InfoRowModule,
     ButtonModule,
     AccountFormModule,
     PassChangeFormModule
--- src/app/pages/main/account-page/organization-details/organization-details.module.ts
+++ src/app/pages/main/account-page/organization-details/organization-details.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrganizationDetailsRoutingModule } from './organization-details-routing.module';
 
+import { InfoRowModule } from 'src/app/shared/modules/info-row/info-row.module';
 import { ButtonModule } from 'src/app/shared/modules/button/button.module';
 import { OrganizationFormModule } from 'src/app/core/modules/forms/organization-form/organization-form.module';
 
@@ -12,6 +13,7 @@ import { OrganizationDetailsPage } from './organization-details.page';
   imports: [
     CommonModule,
     OrganizationDetailsRoutingModule,
+    InfoRowModule,
     ButtonModule,
     OrganizationFormModule
   ]
