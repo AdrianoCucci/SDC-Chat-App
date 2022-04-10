4148e941221297760d25543be6b635b79bc1cfd2
acucci <acucci@edgecollab.com>
2022-04-09 23:17:10 -0400

    Implemented initial notification features.

--- src/app/core/services/notifications/event-notifications-service.ts
+++ src/app/core/services/notifications/event-notifications-service.ts
@@ -6,7 +6,9 @@ import { EventsService } from "src/app/shared/modules/events/events.service";
 import { ChatMessage } from "../../models/messages/chat-message";
 import { RoomPing } from "../../models/room-pings/room-ping";
 import { Room } from "../../models/rooms/room";
+import { NotificationPrefs } from "../../models/user-prefs/notification-prefs.model";
 import { User } from "../../models/users/user";
+import { UserPrefsService } from "../user-prefs.service";
 import { ChatService } from "../web-socket/chat.service";
 import { RoomPingsService } from "../web-socket/room-pings.service";
 import { SwNotificationsService } from "./sw-notifications.service";
@@ -17,46 +19,55 @@ import { SwNotificationsService } from "./sw-notifications.service";
 export class EventNotificationsService implements IDisposable {
   private _subscriptions: EventSubscription[];
 
-  constructor(private _eventsService: EventsService, private _swNotificationsService: SwNotificationsService) { }
+  constructor(
+    private _eventsService: EventsService,
+    private _swNotificationsService: SwNotificationsService,
+    private _userPrefsService: UserPrefsService
+  ) { }
 
   public registerEvents(): void {
     this.dispose();
     const subscriptions: EventSubscription[] = [];
+    const prefs: NotificationPrefs = this._userPrefsService.getPreference("notifications");
 
-    subscriptions.push(this._eventsService.subscribe({
-      eventSources: ChatService.name,
-      eventTypes: "message",
-      eventHandler: (event: Event<ChatMessage>) => {
-        const senderUser: User = event.data.senderUser;
-        const title: string = senderUser
-          ? `${senderUser.displayName || senderUser.username} posted a message`
-          : "New Chat Message";
+    if(prefs?.chatMessages) {
+      subscriptions.push(this._eventsService.subscribe({
+        eventSources: ChatService.name,
+        eventTypes: "message",
+        eventHandler: (event: Event<ChatMessage>) => {
+          const senderUser: User = event.data.senderUser;
+          const title: string = senderUser
+            ? `${senderUser.displayName || senderUser.username} posted a message`
+            : "New Chat Message";
 
-        this._swNotificationsService.showNotification(title, {
-          data: event.data,
-          body: event.data.contents
-        });
-      }
-    }));
+          this._swNotificationsService.showNotification(title, {
+            data: event.data,
+            body: event.data.contents
+          });
+        }
+      }));
+    }
 
-    subscriptions.push(this._eventsService.subscribe({
-      eventSources: RoomPingsService.name,
-      eventTypes: "room-ping-request",
-      eventHandler: (event: Event<RoomPing>) => {
-        const requestUser: User = event.data.requestUser;
-        const room: Room = event.data.room;
+    if(prefs?.roomPings) {
+      subscriptions.push(this._eventsService.subscribe({
+        eventSources: RoomPingsService.name,
+        eventTypes: "room-ping-request",
+        eventHandler: (event: Event<RoomPing>) => {
+          const requestUser: User = event.data.requestUser;
+          const room: Room = event.data.room;
 
-        const title: string = [
-          `${requestUser ? `${requestUser.displayName || requestUser.username} is pinging ` : ""}`,
-          room?.name ?? ""
-        ].join('') ?? "Room Ping";
+          const title: string = [
+            `${requestUser ? `${requestUser.displayName || requestUser.username} is pinging ` : ""}`,
+            room?.name ?? ""
+          ].join('') ?? "Room Ping";
 
-        this._swNotificationsService.showNotification(title, {
-          data: event.data,
-          body: event.data.requestMessage
-        });
-      }
-    }));
+          this._swNotificationsService.showNotification(title, {
+            data: event.data,
+            body: event.data.requestMessage
+          });
+        }
+      }));
+    }
 
     this._subscriptions = subscriptions;
   }
--- src/app/pages/main/account-page/account-page.module.ts
+++ src/app/pages/main/account-page/account-page.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { AccountPageRoutingModule } from './account-page-routing.module';
 
 import { ButtonModule } from 'src/app/shared/modules/button/button.module';
+import { InputToggleModule } from 'src/app/shared/modules/forms/inputs/input-toggle/input-toggle.module';
+import { InlineAlertModule } from 'src/app/shared/modules/inline-alert/inline-alert.module';
 import { AccountFormModule } from 'src/app/core/modules/forms/account-form/account-form.module';
 import { PassChangeFormModule } from 'src/app/core/modules/forms/pass-change-form/pass-change-form.module';
 import { OrganizationFormModule } from 'src/app/core/modules/forms/organization-form/organization-form.module';
@@ -16,6 +18,8 @@ import { AccountPage } from './account-page.component';
     CommonModule,
     AccountPageRoutingModule,
     ButtonModule,
+    InputToggleModule,
+    InlineAlertModule,
     AccountFormModule,
     PassChangeFormModule,
     OrganizationFormModule,
