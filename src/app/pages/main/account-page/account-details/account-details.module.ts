464a246ad76e441e41d5d06a5b9ad566a35575e5
acucci <acucci@edgecollab.com>
2022-04-10 18:33:36 -0400

    Implemented AccountDetailsPage.

--- src/app/pages/main/account-page/account-details/account-details.module.ts
+++ src/app/pages/main/account-page/account-details/account-details.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountDetailsRoutingModule } from './account-details-routing.module';
 
+import { ButtonModule } from 'src/app/shared/modules/button/button.module';
+import { AccountFormModule } from 'src/app/core/modules/forms/account-form/account-form.module';
+import { PassChangeFormModule } from 'src/app/core/modules/forms/pass-change-form/pass-change-form.module';
+
 import { AccountDetailsPage } from './account-details.page';
 
 @NgModule({
   declarations: [AccountDetailsPage],
   imports: [
     CommonModule,
-    AccountDetailsRoutingModule
+    AccountDetailsRoutingModule,
+    ButtonModule,
+    AccountFormModule,
+    PassChangeFormModule
   ]
 })
 export class AccountDetailsModule { }
