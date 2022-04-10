301958b92295089e2ed3a2cca2b9165311b6b65d
acucci <acucci@edgecollab.com>
2022-04-10 19:40:26 -0400

    Implemented AccessibilityPage.

--- src/app/pages/main/account-page/accessibility/accessibility.page.ts
+++ src/app/pages/main/account-page/accessibility/accessibility.page.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { faTextHeight, faCircleHalfStroke } from '@fortawesome/free-solid-svg-icons';
+import { AccessibilityPrefs } from 'src/app/core/models/user-prefs/accessibility-prefs.model';
+import { Pair } from 'src/app/shared/models/pair';
+import { AccessibilityService } from 'src/app/shared/modules/accessibility/accessibility.service';
 
 @Component({
   selector: 'app-accessibility',
   templateUrl: './accessibility.page.html',
   styleUrls: ['./accessibility.page.scss']
 })
-export class AccessibilityPage implements OnInit {
+export class AccessibilityPage implements OnInit, OnDestroy {
+  public readonly fontSizeIcon: IconDefinition = faTextHeight;
+  public readonly themeIcon: IconDefinition = faCircleHalfStroke;
+  public readonly themeOptions: Pair<string, string>[] = [
+    { key: "Default", value: "default" },
+    { key: "High Contrast", value: "high-contrast" }
+  ];
 
-  constructor() { }
+  private _currentPrefs: AccessibilityPrefs;
+
+  constructor(public service: AccessibilityService) { }
 
   ngOnInit(): void {
+    this._currentPrefs = this.service.getPreferences();
   }
 
+  ngOnDestroy(): void {
+    if(!this.isSaving) {
+      this.service.setPreferences(this._currentPrefs);
+    }
+  }
+
+  async onSave(): Promise<void> {
+    await this.service.savePreferences();
+    this._currentPrefs = this.service.getPreferences();
+  }
+
+  public get isSaving(): boolean {
+    return this.service.isSavingPrefs;
+  }
 }
--- src/app/shared/modules/accessibility/accessibility.service.ts
+++ src/app/shared/modules/accessibility/accessibility.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
+import { AccessibilityPrefs } from "src/app/core/models/user-prefs/accessibility-prefs.model";
 import { UserPrefsService } from "src/app/core/services/user-prefs.service";
-import { AccessibilityOptionsModel } from "./accessibility-options.model";
 
 @Injectable({
   providedIn: 'root'
@@ -8,22 +8,38 @@ import { AccessibilityOptionsModel } from "./accessibility-options.model";
 export class AccessibilityService {
   private readonly _prefsKey: string = "accessibility";
 
-  private _model: AccessibilityOptionsModel;
-  private _debounceTimeout: number;
+  private _prefs: AccessibilityPrefs;
+
+  private _isSavingPrefs: boolean = false;
 
   constructor(private _userPrefsService: UserPrefsService) {
     this.loadDefaultPreferences();
   }
 
+  public getPreferences(): AccessibilityPrefs {
+    return { ...this._prefs };
+  }
+
+  public setPreferences(prefs: AccessibilityPrefs) {
+    this._prefs = prefs;
+    this.updateDOM();
+  }
+
   public async savePreferences(): Promise<void> {
-    await this._userPrefsService.setPreference(this._prefsKey, this._model);
+    try {
+      this._isSavingPrefs = true;
+      await this._userPrefsService.setPreference(this._prefsKey, this._prefs);
+    }
+    finally {
+      this._isSavingPrefs = false;
+    }
   }
 
   public loadPreferences(): void {
-    const prefs: AccessibilityOptionsModel = this._userPrefsService.getPreference(this._prefsKey);
+    const prefs: AccessibilityPrefs = this._userPrefsService.getPreference(this._prefsKey);
 
     if(prefs) {
-      this._model = prefs;
+      this._prefs = prefs;
       this.updateDOM();
     }
     else {
@@ -31,38 +47,36 @@ export class AccessibilityService {
     }
   }
 
-  private debounceSavePreferences(): void {
-    this.updateDOM();
-
-    window.clearTimeout(this._debounceTimeout);
-    this._debounceTimeout = window.setTimeout(() => this.savePreferences(), 500);
-  }
-
   public loadDefaultPreferences(): void {
-    this._model = { theme: "default" };
+    this._prefs = { theme: "default" };
     this.updateDOM();
   }
 
-  public updateDOM(): void {
+  public updateDOM(prefs?: AccessibilityPrefs): void {
+    prefs = prefs ?? this._prefs;
     const root: HTMLHtmlElement = document.querySelector("html");
 
-    root.style.fontSize = this._model.fontSize != null ? `${this._model.fontSize}px` : null;
-    this._model.theme ? root.setAttribute("theme", this._model.theme) : root.removeAttribute("theme");
+    root.style.fontSize = prefs.fontSize != null ? `${prefs.fontSize}px` : null;
+    prefs.theme ? root.setAttribute("theme", prefs.theme) : root.removeAttribute("theme");
   }
 
   public get fontSize(): number {
-    return this._model.fontSize;
+    return this._prefs.fontSize;
   }
   public set fontSize(value: number) {
-    this._model.fontSize = value;
-    this.debounceSavePreferences();
+    this._prefs.fontSize = value;
+    this.updateDOM();
   }
 
   public get theme(): string {
-    return this._model.theme;
+    return this._prefs.theme;
   }
   public set theme(value: string) {
-    this._model.theme = value;
-    this.debounceSavePreferences();
+    this._prefs.theme = value;
+    this.updateDOM();
+  }
+
+  public get isSavingPrefs(): boolean {
+    return this._isSavingPrefs;
   }
 }
