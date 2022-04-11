ef177432c10f80d39b3165fcafd4c0c4e8065f0d
acucci <acucci@edgecollab.com>
2022-04-10 20:03:31 -0400

    Removed unused features.

--- src/app/pages/main/account-page/account-page.module.ts
+++ src/app/pages/main/account-page/account-page.module.ts
@@ -3,13 +3,6 @@ import { CommonModule } from '@angular/common';
 import { AccountPageRoutingModule } from './account-page-routing.module';
 
 import { TabMenuPanelModule } from 'src/app/shared/modules/menus/tab-menu-panel/tab-menu-panel.module';
-import { ButtonModule } from 'src/app/shared/modules/button/button.module';
-import { InputToggleModule } from 'src/app/shared/modules/forms/inputs/input-toggle/input-toggle.module';
-import { InlineAlertModule } from 'src/app/shared/modules/inline-alert/inline-alert.module';
-import { AccountFormModule } from 'src/app/core/modules/forms/account-form/account-form.module';
-import { PassChangeFormModule } from 'src/app/core/modules/forms/pass-change-form/pass-change-form.module';
-import { OrganizationFormModule } from 'src/app/core/modules/forms/organization-form/organization-form.module';
-import { AccessibilityDialogModule } from 'src/app/shared/modules/accessibility/accessibility-dialog/accessibility.dialog.module';
 
 import { AccountPage } from './account-page.component';
 
@@ -18,14 +11,7 @@ import { AccountPage } from './account-page.component';
   imports: [
     CommonModule,
     AccountPageRoutingModule,
-    TabMenuPanelModule,
-    ButtonModule,
-    InputToggleModule,
-    InlineAlertModule,
-    AccountFormModule,
-    PassChangeFormModule,
-    OrganizationFormModule,
-    AccessibilityDialogModule
+    TabMenuPanelModule
   ]
 })
 export class AccountPageModule { }
