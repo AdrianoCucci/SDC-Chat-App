c89a67b9788911e569017258684cc4c8376c9809
acucci <acucci@edgecollab.com>
2022-04-10 20:02:17 -0400

    Implemented NotificationsPage.

--- src/app/pages/main/account-page/notifications/notifications.module.ts
+++ src/app/pages/main/account-page/notifications/notifications.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotificationsRoutingModule } from './notifications-routing.module';
 
+import { InfoRowModule } from 'src/app/shared/modules/info-row/info-row.module';
+import { InputToggleModule } from 'src/app/shared/modules/forms/inputs/input-toggle/input-toggle.module';
+import { ButtonModule } from 'src/app/shared/modules/button/button.module';
+import { InlineAlertModule } from 'src/app/shared/modules/inline-alert/inline-alert.module';
+import { LoaderModule } from 'src/app/shared/modules/overlays/loader/loader.module';
+
 import { NotificationsPage } from './notifications.page';
 
 @NgModule({
   declarations: [NotificationsPage],
   imports: [
     CommonModule,
-    NotificationsRoutingModule
+    NotificationsRoutingModule,
+    InfoRowModule,
+    InputToggleModule,
+    ButtonModule,
+    InlineAlertModule,
+    LoaderModule
   ]
 })
 export class NotificationsModule { }
